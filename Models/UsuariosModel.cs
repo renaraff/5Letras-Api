27073b0aec14f6569a55873c88788e1d1dd8d620
renaraff <refurtado2007@gmail.com>
2024-10-01 11:52:30 -0300

    Adicionar arquivos de projeto.

---
+++ Models/UsuariosModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Api.Models
+{
+    public class UsuariosModel
+    {
+        public int UsuarioId { get; set; }
+
+        public string UsuarioNome { get; set; } = string.Empty;
+
+        public string UsuarioEmail { get; set; } = string.Empty;
+
+        public string UsuarioSenha { get; set; } = string.Empty;
+
+        public string UsuarioGraduacao { get; set; } = string.Empty;
+
+        public int TipoUsuarioId { get; set; }
+
+        public static implicit operator List<object>(UsuariosModel v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repositorios/Interfaces/IUsuariosRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using Api.Models;
+
+namespace Api.Repositorios.Interfaces
+{
+    public interface IUsuariosRepositorio
+    {
+        Task<List<UsuariosModel>> GetAll();
+
+        Task<UsuariosModel> GetById(int id);
+
+        Task<UsuariosModel> InsertUser(UsuariosModel user);
+
+        Task<UsuariosModel> UpdateUser(UsuariosModel user, int id);
+
+        Task<bool> DeleteUser(int id);
+
+        Task<UsuariosModel> Login(string email, string senha);
+    }
+}
